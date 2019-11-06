b5c437a6fc42b29bcf6e5ebd66d247baefcbc684
Germano Lodi <lodi@MacBook-Pro-de-ateliware.local>
2019-11-06 15:37:19 -0300

    CORS Policy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import pika, os
 
 app = Flask(__name__)
+CORS(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
